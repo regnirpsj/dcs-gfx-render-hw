7890bc7f897d07a86810e963a26d52220299c23c
jan p springer <jsd@igroup.org>
2016-12-04 12:24:07 +0000

    added: top-level header compile test

---
+++ src/lib/render/vulkan/test/compile.cpp
@@ -0,0 +1,48 @@
+// -*- Mode:C++ -*-
+
+/**************************************************************************************************/
+/*                                                                                                */
+/* Copyright (C) 2016 University of Hull                                                          */
+/*                                                                                                */
+/**************************************************************************************************/
+/*                                                                                                */
+/*  module     :  hugh/render/vulkan/test/compile.cpp                                             */
+/*  project    :                                                                                  */
+/*  description:                                                                                  */
+/*                                                                                                */
+/**************************************************************************************************/
+
+// includes, system
+
+//#include <>
+
+// includes, project
+
+#include <hugh/render/vulkan/allocator.hpp>
+#include <hugh/render/vulkan/context.hpp>
+#include <hugh/render/vulkan/handle.hpp>
+#include <hugh/render/vulkan/io.hpp>
+
+#define HUGH_USE_TRACE
+#undef HUGH_USE_TRACE
+#include <hugh/support/trace.hpp>
+
+// internal unnamed namespace
+
+namespace {
+  
+  // types, internal (class, enum, struct, union, typedef)
+
+  // variables, internal
+  
+  // functions, internal
+
+} // namespace {
+
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(test_hugh_render_vulkan_compile)
+{
+  BOOST_CHECK(true);
+}
