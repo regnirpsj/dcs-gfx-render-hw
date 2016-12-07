3a8dc48a6163bb20604fef66512e0268632e0e35
jan p springer <jsd@igroup.org>
2016-12-07 20:00:46 +0000

    added: top-level header

---
+++ inc/hugh/render/vulkan.hpp
@@ -0,0 +1,52 @@
+// -*- Mode:C++ -*-
+
+/**************************************************************************************************/
+/*                                                                                                */
+/* Copyright (C) 2016 University of Hull                                                          */
+/*                                                                                                */
+/**************************************************************************************************/
+/*                                                                                                */
+/*  module     :  hugh/render/vulkan.hpp                                                          */
+/*  project    :                                                                                  */
+/*  description:                                                                                  */
+/*                                                                                                */
+/**************************************************************************************************/
+
+#if !defined(HUGH_RENDER_VULKAN_HPP)
+
+#define HUGH_RENDER_VULKAN_HPP
+
+// includes, system
+
+//#include <>
+
+// includes, project
+
+#include <hugh/render/vulkan/adapter.hpp>
+#include <hugh/render/vulkan/allocator.hpp>
+#include <hugh/render/vulkan/context.hpp>
+#include <hugh/render/vulkan/handle.hpp>
+#include <hugh/render/vulkan/instance.hpp>
+#include <hugh/render/vulkan/io.hpp>
+
+namespace hugh {
+  
+  namespace render {
+
+    namespace vulkan {
+
+      // types, exported (class, enum, struct, union, typedef)
+
+      // variables, exported (extern)
+
+      // functions, inlined (inline)
+  
+      // functions, exported (extern)
+
+    } // namespace vulkan {
+
+  } // namespace render {
+  
+} // namespace hugh {
+
+#endif // #if !defined(HUGH_RENDER_VULKAN_HPP)
